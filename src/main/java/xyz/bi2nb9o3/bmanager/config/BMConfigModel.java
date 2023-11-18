2f96ee75e12dfc945d643a1a57ecbb8f9c6a4e42
Bi2Nb9O3 <me@bi2nb9o2.xyz>
2023-11-18 23:03:19 +0800

    A Big Step!Wait for translate

--- src/main/java/xyz/bi2nb9o3/bmanager/config/BMConfigModel.java
+++ src/main/java/xyz/bi2nb9o3/bmanager/config/BMConfigModel.java
@@ -2,9 +2,31 @@ package xyz.bi2nb9o3.bmanager.config;
 
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
+import io.wispforest.owo.config.annotation.SectionHeader;
+
+import java.awt.*;
+import java.util.Dictionary;
 
 @Modmenu(modId = "bmanager")
 @Config(name = "bManager",wrapperName = "BMConfig")
 public class BMConfigModel {
     public boolean enableMod=true;
+
+    @SectionHeader("Database Connection")
+
+    public String jdbcURL="/Default/ JDBC URL";
+
+    public String dbUsername="/Default/ Username";
+
+    public String dbPassword="/Default/ Password";
+
+    @SectionHeader("Database Selector")
+
+    public DataUseAsIdChoices DataUseAsId=DataUseAsIdChoices.PlayerName;
+
+    public enum DataUseAsIdChoices {
+        ClientUUID, PlayerName;
+    }
+
+    public String showDetailsJson="[{'table':'1','row':'2',show:['Player Money','$']}]";
 }
